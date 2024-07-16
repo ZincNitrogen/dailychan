1723b21ec998d7c2ff56cc26ef9f8edc780564d9
ZincNitrogen <zdn313@yahoo.com>
2024-07-16 16:58:40 -0400

    fixed previous issue - frontend recieved unique info from backend. Need to implement frontend cleaning function that keeps only one piece of info at a time.

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,17 @@
 
 const postContainer = document.querySelector('.post-container');
 const url = "http://localhost:8000/ServerSideRequest";
+const newPostBtn = document.querySelector(`.btn`);
+let postContainerChildern = postContainer.childNodes;
 
 
 
-async function pingProxy() {
-    const response = await fetch(url);
+
+
+
+async function pingProxy(source) {
+
+    const response = await fetch(source);
     const fourchanData = await response.json(); 
     const usableFourChanData = JSON.parse(fourchanData);
 
@@ -39,17 +45,65 @@ async function pingProxy() {
     postContainer.append(paintName);
     postContainer.append(paintCom);
 
-
-   
-
+}
 
 
+
+
+async function pingProxyTwo(source) {
+
+    const response = await fetch(source);
+    const fourchanData = await response.text(); 
+    const usableFourChanData = fourchanData;
+
+
+    //console.log(fourchanData);
+    console.log(usableFourChanData);
+    console.log(usableFourChanData.Board);
+    console.log(usableFourChanData.Post);
+
+    //console.log(usableFourChanData[Board]);
+    //console.log(usableFourChanData[1]);
+
+    let one = document.createElement("div");
+    let two = document.createElement("div");
+    let three = document.createElement("div");
+    let four = document.createElement("div");
+    let five = document.createElement("div");
+
+
+    one.prepend(usableFourChanData.Board);
+    two.prepend(usableFourChanData.Post.no);
+    three.prepend(usableFourChanData.Post.now);
+    four.prepend(usableFourChanData.Post.name);
+    five.prepend(usableFourChanData.Post.com);
+
+
+
+    postContainer.append(one);
+    postContainer.append(two);
+    postContainer.append(three);
+    postContainer.append(four);
+    postContainer.append(five);
 
 }
 
 
-pingProxy();
+
+//pingProxy(url);
+
+
+//button must call function and refesh page
+newPostBtn.addEventListener("pointerup", (e) => {
+
+    //deleteChildren(postContainer);
+    pingProxy(url);
+
+    // location.reload();
+    // return false;
+})
 
 
 
-//certan undefined 'com' values i suspect are images. Remember to add image support from backend to frontend. 
+//TODO: Remember to add image support from backend to frontend. 
+//TODO: FIGURE OUT HOW TO GET UNIQUE VALUES FROM BACKEND
